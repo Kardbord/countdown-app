f05d68c45f0420a3a2909e855bb742edc6892df7
Kardbord <tanner.kvarfordt@gmail.com>
2018-06-25 10:54:45 -0600

    Added skew styles and title

--- src/components/app.js
+++ src/components/app.js
@@ -1,10 +1,17 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 export default class App extends Component {
   render() {
     return (
-      <div>
-        <h1>Birthday Countdown</h1>
+      <div className="grid">
+        <h1 className="grid__title">Birthday Countdown</h1>
+        <div className="grid__skew-dark-one-box" />
+        <div className="grid__skew-dark-two" />
+        <div className="grid__skew-dark-three" />
+
+        <div className="grid__skew-light-one" />
+        <div className="grid__skew-light-two" />
+        <div className="grid__skew-light-three-box" />
       </div>
     );
   }
