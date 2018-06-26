4a62f3c96ec2d6ff70af761b69a0f070d7ca7ee5
Kardbord <tanner.kvarfordt@gmail.com>
2018-06-26 09:23:49 -0600

    rendering jsx based on app state

--- src/components/button.js
+++ src/components/button.js
@@ -1,11 +1,11 @@
-import React from 'react';
+import React from "react";
 
-const Button = (title) => {
-    return (
-        <button className="button">
-            {title}
-        </button>
-    )
-}
+const Button = (title, callback) => {
+  return (
+    <button className="button" onClick={callback}>
+      {title}
+    </button>
+  );
+};
 
 export default Button;
