c1bfb69e837da9843f1018d76694883e0b7482bb
Kardbord <tanner.kvarfordt@gmail.com>
2018-06-27 09:19:40 -0600

    Generate and date selection is now functional. Logs to console for now

--- src/components/app.js
+++ src/components/app.js
@@ -6,44 +6,88 @@ import Clock from "./clock";
 import ChangeDate from "./changeDate";
 import LargeText from "./largeText";
 
+import moment from "moment";
+
 export default class App extends Component {
-  constructor(props) {
-    super(props);
+    constructor(props) {
+        super(props);
 
-    this.state = {
-      active: false
-    };
-  }
-
-  renderItems = function() {
-    if (this.state.active) {
-      return [
-        <Clock />,
-        ChangeDate("Change Date", () => this.setState({ active: false })),
-        LargeText("04/03"),
-        <label className="grid__remaining">Remaining until your selected date.</label>
-      ];
-    } else {
-      return [
-        Button("Generate Countdown", () => this.setState({ active: true })),
-        <Picker />
-      ];
+        this.state = {
+            active: false,
+            startDate: moment()
+        };
     }
-  }.bind(this);
 
-  render() {
-    return (
-      <div className="grid">
-        <h1 className="grid__title">Date Countdown</h1>
-        <div className="grid__skew-dark-two" />
-        <div className="grid__skew-dark-three" />
+    handleChange = function(date) {
+        this.setState({
+            startDate: date
+        });
+    }.bind(this);
 
-        <div className="grid__skew-light-one" />
-        <div className="grid__skew-light-two" />
-        <div className="grid__skew-light-three-box" />
+    handleGenerate = function() {
+        this.setState({ active: true });
 
-        {this.renderItems()}
-      </div>
-    );
-  }
+        let countDownDate = this.state.startDate.toDate().getTime();
+
+        // Update the countdown every one second
+        let x = setInterval(function() {
+            let now = new Date().getTime();
+
+            let distance = countDownDate - now;
+
+            let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+            let hours = Math.floor(
+                (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+            );
+            let minutes = Math.floor(
+                (distance % (1000 * 60 * 60)) / (1000 * 60)
+            );
+            let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+            const time =
+                days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+
+            console.log(time);
+
+            if (distance < 0) {
+                clearInterval(x);
+            }
+        }, 1000);
+    }.bind(this);
+
+    renderItems = function() {
+        if (this.state.active) {
+            return [
+                <Clock />,
+                ChangeDate("Change Date", () =>
+                    this.setState({ active: false })
+                ),
+                LargeText("04/03"),
+                <label className="grid__remaining">
+                    Remaining until your selected date.
+                </label>
+            ];
+        } else {
+            return [
+                Button("Generate Countdown", this.handleGenerate),
+                <Picker callback={date => this.handleChange(date)} />
+            ];
+        }
+    }.bind(this);
+
+    render() {
+        return (
+            <div className="grid">
+                <h1 className="grid__title">Date Countdown</h1>
+                <div className="grid__skew-dark-two" />
+                <div className="grid__skew-dark-three" />
+
+                <div className="grid__skew-light-one" />
+                <div className="grid__skew-light-two" />
+                <div className="grid__skew-light-three-box" />
+
+                {this.renderItems()}
+            </div>
+        );
+    }
 }
--- src/components/picker.js
+++ src/components/picker.js
@@ -3,31 +3,32 @@ import React, { Component } from "react";
 import DatePicker from "react-datepicker";
 import moment from "moment";
 
-import 'react-datepicker/dist/react-datepicker.css';
+import "react-datepicker/dist/react-datepicker.css";
 
 export default class Picker extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      startDate: moment()
-    };
-    this.handleChange = this.handleChange.bind(this);
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            startDate: moment()
+        };
+        this.handleChange = this.handleChange.bind(this);
+    }
 
-  handleChange(date) {
-    this.setState({
-      startDate: date
-    });
-  }
+    handleChange(date) {
+        this.setState({
+            startDate: date
+        });
+        this.props.callback(date);
+    }
 
-  render() {
-    return (
-      <div className="picker">
-        <DatePicker
-          selected={this.state.startDate}
-          onChange={this.handleChange}
-        />
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="picker">
+                <DatePicker
+                    selected={this.state.startDate}
+                    onChange={this.handleChange}
+                />
+            </div>
+        );
+    }
 }
